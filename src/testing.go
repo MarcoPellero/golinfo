b8dbbdf086269fd6ff67636efa29f0103fdda644
Marco <marco.pellero.it@gmail.com>
2023-03-22 00:00:28 +0100

    wrote utility to get avg score per sub and task

--- src/testing.go
+++ src/testing.go
@@ -37,7 +37,52 @@ func avgSubs(username, password string) float64 {
 	return float64(sum) / float64(len(profile.Scores))
 }
 
+func avgScorePerTask(username, password string) (float64, float64) {
+	// gets (avg number of subs per task, and per task)
+	token, err := Login(username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	profile, err := GetProfile(username)
+	if err != nil {
+		panic(err)
+	}
+
+	var sum uint64 = 0
+	subMeanResults := make(chan float64, len(profile.Scores))
+
+	for _, task := range profile.Scores {
+		go func(task ProfileScore) {
+			info, err := GetTaskSubmissions(task.Name, token)
+			if err != nil {
+				panic(err)
+			}
+
+			scoreSum := 0
+			for _, x := range info {
+				scoreSum += x.Score
+			}
+
+			subMeanResults <- float64(scoreSum) / float64(len(info))
+		}(task)
+	}
+
+	for range profile.Scores {
+		sum += uint64(<-subMeanResults)
+	}
+	subMean := float64(sum) / float64(len(profile.Scores))
+
+	sum = 0
+	for _, x := range profile.Scores {
+		sum += uint64(x.Score)
+	}
+	taskMean := float64(sum) / float64(len(profile.Scores))
+
+	return subMean, taskMean
+}
+
 func main() {
-	x := avgSubs(os.Args[1], os.Args[2])
-	fmt.Println(x)
+	a, b := avgScorePerTask(os.Args[1], os.Args[2])
+	fmt.Println(a, b)
 }
