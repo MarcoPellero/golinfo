7686970c012051871ed9ed341055da87e754603a
Marco <marco.pellero.it@gmail.com>
2023-03-21 23:50:51 +0100

    wrote utility to get avg number of subs/task

--- src/testing.go
+++ src/testing.go
@@ -2,10 +2,43 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
+func avgSubs(username, password string) float64 {
+	// same as above but concurrent
+
+	token, err := Login(username, password)
+	if err != nil {
+		panic(err)
+	}
+
+	profile, err := GetProfile(username)
+	if err != nil {
+		panic(err)
+	}
+
+	var sum uint64 = 0
+	results := make(chan int, len(profile.Scores))
+
+	for _, task := range profile.Scores {
+		go func(task ProfileScore) {
+			info, err := GetTaskSubmissions(task.Name, token)
+			if err != nil {
+				panic(err)
+			}
+			results <- len(info)
+		}(task)
+	}
+
+	for range profile.Scores {
+		sum += uint64(<-results)
+	}
+
+	return float64(sum) / float64(len(profile.Scores))
+}
+
 func main() {
-	profile, err := GetProfile("marco_pellero")
-	fmt.Println(err)
-	fmt.Println(profile)
+	x := avgSubs(os.Args[1], os.Args[2])
+	fmt.Println(x)
 }
